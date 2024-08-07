GiorgosXou/untappdscr

commit cf360cab342c66cf0b02f5027167088a4c392313

`example_5` - Reduced redundant `date_color`-range, to see if this fixes colors for MS-Excel and for storage size

examples/example_5.py
@@ -100,7 +100,7 @@ def print_breweries(_from, at, color='#000000'): # repeating code but nvm for no
     coln       = col2num(col)
     cole       = num2col(coln+13)
     breweries  = get_list_table(_from, True) # random choice or something
-    date_color = ColorHash(datetime.now()).hex.split('#')[1]
+    date_color = ColorHash(datetime.now().second).hex.split('#')[1]
     color      = color.split('#')[1]
     sheet.move_range(f'{col}{row}:{cole}{sheet.max_row}', rows=len(breweries), cols=0)
     for i, brewery in enumerate(breweries,row):
@@ -130,7 +130,7 @@ def print_beers(_from, at): # repeating code but nvm for now
     coln = col2num(col)
     cole = num2col(coln+12)
     beers  = get_list_table(_from, True) # random choice or something
-    date_color = ColorHash(datetime.now()).hex.split('#')[1] # unnecessery computation here but nvm, could had been just random or just boolean based, lol
+    date_color = ColorHash(datetime.now().second).hex.split('#')[1] # unnecessery computation here but nvm, could had been just random or just boolean based, lol
     sheet.move_range(f'{col}{row}:{cole}{sheet.max_row}', rows=len(beers), cols=0)
     for i, beer in enumerate(beers, row):
         beer      = untappd.get_beer(int(beer)) # int lol
@@ -158,7 +158,7 @@ def print_venues(_from, at):
     coln       = col2num(col)
     cole       = num2col(coln+10)
     venue      = get_list_table(_from, True) # random choice or something
-    date_color = ColorHash(datetime.now()).hex.split('#')[1] # unnecessery computation here but nvm, could had been just random or just boolean based, lol
+    date_color = ColorHash(datetime.now().second).hex.split('#')[1] # unnecessery computation here but nvm, could had been just random or just boolean based, lol
     sheet.move_range(f'{col}{row}:{cole}{sheet.max_row}', rows=len(venue), cols=0)
     for i, venue in enumerate(venue, row):
         venue = untappd.get_venue(int(venue)) # int lol
@@ -182,7 +182,7 @@ def print_top_rated_beers(at, country='', type='', get_picker=False): # col, row
     coln       = col2num(col)
     cole       = num2col(coln+9)
     beers      = untappd.get_top_rated_beers(country, type, get_picker).items()
-    date_time  = datetime.now().strftime(DATE_FORMAT)
+    date_time  = datetime.now().second.strftime(DATE_FORMAT)
     date_color = ColorHash(date_time).hex.split('#')[1]   
     sheet.move_range(f'{col}{row}:{cole}{sheet.max_row}', rows=len(beers), cols=0)
     for i,(_, beer) in enumerate(beers,row):
@@ -209,7 +209,7 @@ def print_top_rated_breweries(at, country='', type='', color='#217346', get_pick
     coln       = col2num(col)
     cole       = num2col(coln+7)
     breweries  = untappd.get_top_rated_breweries(country, type, get_picker).items()
-    date_time  = datetime.now().strftime(DATE_FORMAT)
+    date_time  = datetime.now().second.strftime(DATE_FORMAT)
     date_color = ColorHash(date_time).hex.split('#')[1]
     sheet.move_range(f'{col}{row}:{cole}{sheet.max_row}', rows=len(breweries), cols=0)
     for i,(_, brewery) in enumerate(breweries,row):

commit 73df042c0c0cb7ff38073bb039a2822e9fa49d2e

FIXED: `example_5` datetime issue introduced with cf360cab

examples/example_5.py
@@ -181,8 +181,9 @@ def print_top_rated_beers(at, country='', type='', get_picker=False): # col, row
     coln       = col2num(col)
     cole       = num2col(coln+9)
     beers      = untappd.get_top_rated_beers(country, type, get_picker).items()
-    date_time  = datetime.now().second.strftime(DATE_FORMAT)
-    date_color = ColorHash(date_time).hex.split('#')[1]   
+    date_now   = datetime.now()
+    date_time  = date_now.strftime(DATE_FORMAT)
+    date_color = ColorHash(date_now.second).hex.split('#')[1]
     sheet.move_range(f'{col}{row}:{cole}{sheet.max_row}', rows=len(beers), cols=0)
     for i,(_, beer) in enumerate(beers,row):
         sheet.cell(i, coln     ).value = beer.details.discontinued
@@ -208,8 +209,9 @@ def print_top_rated_breweries(at, country='', type='', color='#217346', get_pick
     coln       = col2num(col)
     cole       = num2col(coln+7)
     breweries  = untappd.get_top_rated_breweries(country, type, get_picker).items()
-    date_time  = datetime.now().second.strftime(DATE_FORMAT)
-    date_color = ColorHash(date_time).hex.split('#')[1]
+    date_now   = datetime.now()
+    date_time  = date_now.strftime(DATE_FORMAT)
+    date_color = ColorHash(date_now.second).hex.split('#')[1]
     sheet.move_range(f'{col}{row}:{cole}{sheet.max_row}', rows=len(breweries), cols=0)
     for i,(_, brewery) in enumerate(breweries,row):
         sheet.cell(i, coln    ).value = brewery.details.beer_count